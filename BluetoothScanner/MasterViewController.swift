bbaa08672bfb0ba074e06577b9126de6fa217647
Sebastien Arbogast <sebastien.arbogast@gmail.com>
2019-09-06 18:28:28 +0100

    Basic sample

--- BluetoothScanner/MasterViewController.swift
+++ BluetoothScanner/MasterViewController.swift
@@ -7,50 +7,28 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 class MasterViewController: UITableViewController {
 
-    var detailViewController: DetailViewController? = nil
-    var objects = [Any]()
-
+    var discoveredPeripherals = [CBPeripheral]()
+    var connectedPeripherals = [CBPeripheral]()
+    var peripheralsSupportingDeviceInformation = [CBPeripheral]()
+    
+    private let scannedServices: [CBUUID]? = [CBUUID(string: "180A")]//nil
+    
+    private var scanning = false
+    
+    private var centralManager: CBCentralManager?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         navigationItem.leftBarButtonItem = editButtonItem
-
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
-        navigationItem.rightBarButtonItem = addButton
-        if let split = splitViewController {
-            let controllers = split.viewControllers
-            detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
-        }
     }
 
     override func viewWillAppear(_ animated: Bool) {
-        clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
         super.viewWillAppear(animated)
-    }
-
-    @objc
-    func insertNewObject(_ sender: Any) {
-        objects.insert(NSDate(), at: 0)
-        let indexPath = IndexPath(row: 0, section: 0)
-        tableView.insertRows(at: [indexPath], with: .automatic)
-    }
-
-    // MARK: - Segues
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "showDetail" {
-            if let indexPath = tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
-                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
-                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
-                controller.navigationItem.leftItemsSupplementBackButton = true
-            }
-        }
     }
 
     // MARK: - Table View
@@ -60,14 +38,20 @@ class MasterViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return discoveredPeripherals.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let peripheral = discoveredPeripherals[indexPath.row]
+        cell.textLabel!.text = peripheral.identifier.uuidString
+        cell.detailTextLabel!.text = peripheral.name ?? ""
+        if self.peripheralsSupportingDeviceInformation.contains(peripheral) {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
         return cell
     }
 
@@ -75,16 +59,65 @@ class MasterViewController: UITableViewController {
         // Return false if you do not want the specified item to be editable.
         return true
     }
+    
+    @IBAction func scanTapped(_ sender: Any) {
+        if !scanning {
+            self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Stop", style: .plain, target: self, action: #selector(scanTapped(_:)))
+            self.discoveredPeripherals.removeAll()
+            self.connectedPeripherals.removeAll()
+            self.peripheralsSupportingDeviceInformation.removeAll()
+            self.centralManager = CBCentralManager(delegate: self, queue: nil)
+        } else {
+            self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Scan", style: .plain, target: self, action: #selector(scanTapped(_:)))
+            self.centralManager?.stopScan()
+            self.centralManager = nil
+        }
+        
+    }
+}
 
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            objects.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+extension MasterViewController: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            central.scanForPeripherals(withServices: scannedServices, options: [CBCentralManagerScanOptionAllowDuplicatesKey: false])
         }
     }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if discoveredPeripherals.contains(where: { periph -> Bool in
+            periph.identifier.uuidString == peripheral.identifier.uuidString
+        }) {
+            return
+        }
+        self.discoveredPeripherals.append(peripheral)
+        self.tableView.reloadData()
+        central.connect(peripheral, options: nil)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        self.connectedPeripherals.append(peripheral)
+        peripheral.delegate = self
+        peripheral.discoverServices(scannedServices)
+    }
+}
 
-
+extension MasterViewController: CBPeripheralDelegate {
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        if let error = error {
+            print("Service discovery error: \(String(describing: error))")
+        } else {
+            if let services = peripheral.services, services.contains(where: { service -> Bool in
+                service.uuid.uuidString == "180A"
+            }) {
+                self.peripheralsSupportingDeviceInformation.append(peripheral)
+                self.tableView.reloadData()
+            } else if let services = peripheral.services {
+                let serviceList = services.map { service -> String in
+                    return service.uuid.uuidString
+                }
+                print("Services supported by \(peripheral.description): \(serviceList.joined(separator: ", "))")
+            }
+        }
+    }
 }
 
